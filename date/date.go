d77da4d87bc8f62e2ac277e1421dce947e2aeb13
Ryosei Sato <sary.sas.202124@gmail.com>
2026-09-18 09:19:13 +0900

    date: add a go:generate directive
    The date package had no go:generate directive, so neither go generate
    nor the gen program in the root of the repository regenerated its
    tables; they had to be regenerated by running gen.go by hand.
    
    Updates golang/go#80753
    
    Change-Id: Ie5851019767476558d800dfcc831b50a2170d70e

---
+++ date/date.go
@@ -0,0 +1,9 @@
+// Copyright 2026 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:generate go run gen.go
+
+// Package date holds the date and time data of CLDR. It is under construction
+// and does not have an API yet.
+package date
